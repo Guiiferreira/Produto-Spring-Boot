a6ae06c697f1e9c8e0243feba1a868ce9b8ceb10
Guilherme Ferreira <guilhermepereiraferreira4@gmail.com>
2025-05-17 17:54:00 -0300

    Hello World com Spring Boot

--- src/main/java/com/GuilhermeF/produtosapi/ProdutosApiApplication.java
+++ src/main/java/com/GuilhermeF/produtosapi/ProdutosApiApplication.java
@@ -4,9 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@RestController
 public class ProdutosApiApplication {
 
+	/* @GetMapping("/hello-world")// mapeando o retorno do metodo para uma chamada GET
+	public String helloWord(){
+		return "Hello World";
+	}*/
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(ProdutosApiApplication.class, args);
 	}
 
